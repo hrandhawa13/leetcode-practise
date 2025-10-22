34301977591a04175563b07c02414c1383aa6cf0
harmanjit.randhawa <harmanjit.randhawa98@gmail.com>
2025-10-21 19:15:24 -0600

    longest substring

---
+++ 5-max-subarray.js
@@ -0,0 +1,22 @@
+// 3. Longest Substring Without Repeating Characters
+
+//Given a string s, find the length of the longest substring without duplicate characters.
+
+
+var lengthOfLongestSubstring = function(s) {
+    let left =0, right=0, result =0;
+    let set = new Set();
+    while(right< s.length){
+        if(set.has(s.charAt(right)))
+            set.delete(s.charAt(left))
+
+        set.add(s.charAt(right))
+        result = Math.max(result, set.size)
+        right++;
+    }
+    return result;
+}
+
+console.log(lengthOfLongestSubstring("abcabcbb"));
+console.log(lengthOfLongestSubstring("bbbbb"));
+console.log(lengthOfLongestSubstring("pwwkew"));
